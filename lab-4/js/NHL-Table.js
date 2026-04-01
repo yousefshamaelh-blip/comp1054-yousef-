adec7acfe5e59b4a73338aff23715150783a5973
yousefshamaelh-blip <yousef.shamaelh@gmail.com>
2026-04-01 19:35:53 -0400

    lab 4 done and other stuff

--- lab-4/js/NHL-Table.js
+++ lab-4/js/NHL-Table.js
@@ -1,3 +1,2 @@
-// STEP 11: Call to tablesorter script here
 const consoleTable = document.querySelector("table");
 let sortableTable = new Tablesort(consoleTable);
--- lesson-10/js/accordion2.js
+++ lesson-10/js/accordion2.js
@@ -1,18 +1,24 @@
-// STEP 6: Nab all the SUMMARY elements
+// STEP 6: Nab all the details elements
+const details = document.querySelectorAll("details")
 
 
 // STEP 7: Loop through the element array, and add an event listener for the toggle event
-
-	
+details.forEach((detail) => {
+	//console.log(detail);
+	detail.addEventListener("toggle",(event) => {
+		console.log("toggle!" + event);
 		// STEP 8a: Check to see whether the DETAILS event that was toggled has the "open" attribute
-		
+		if (event.target.open) {
 			// STEP 8b: The browser just added the "open" attribute, so this DETAILS element was previously closed
-			
+			details.forEach((detail) => {
 				// STEP 8c: If this DETAILS element is NOT the one that was just clicked, close it by removing the "open" attribute
-				
+				if (detail !== event.target) {
+					detail.removeAttribute("open")
+				}
 			
 		
-	
-
-
+			});
+		}
+	});
+});
 // This experiment based on https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details
